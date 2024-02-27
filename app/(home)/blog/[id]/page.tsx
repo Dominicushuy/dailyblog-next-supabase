fa9a8a0437c1389816d5493d793f5f61268b5e8b
Dominicushuy <huypro1996z@gmail.com>
2024-02-27 21:13:07 +0700

    update metadata

--- app/(home)/blog/[id]/page.tsx
+++ app/(home)/blog/[id]/page.tsx
@@ -11,26 +11,26 @@ import Content from './components/Content'
 //   return blogs
 // }
 
-// export async function generateMetadata({ params }: { params: { id: string } }) {
-//   const { data: blog } = (await fetch(
-//     process.env.SITE_URL + '/api/blog?id=' + params.id
-//   ).then((res) => res.json())) as { data: IBlog }
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { data: blog } = (await fetch(
+    process.env.SITE_URL + '/api/blog?id=' + params.id
+  ).then((res) => res.json())) as { data: IBlog }
 
-//   return {
-//     title: blog?.title,
-//     authors: {
-//       name: 'chensokheng',
-//     },
-//     openGraph: {
-//       title: blog?.title,
-//       url: 'https://dailyblog-demo.vercel.app/blog' + params.id,
-//       siteName: 'Daily Blog',
-//       images: blog?.image_url,
-//       type: 'website',
-//     },
-//     keywords: ['daily web coding', 'chensokheng', 'dailywebcoding'],
-//   }
-// }
+  return {
+    title: blog?.title,
+    authors: {
+      name: 'Dominicushuy',
+    },
+    openGraph: {
+      title: blog?.title,
+      url: 'https://dailyblog-next-supabase.vercel.app/blog' + params.id,
+      siteName: 'Daily Blog',
+      images: blog?.image_url,
+      type: 'website',
+    },
+    keywords: ['Dominicushuy', 'nextjs', 'supabase', 'blog'],
+  }
+}
 
 export default async function page({ params }: { params: { id: string } }) {
   const { data: blog } = (await fetch(
